35b6bd60ec97a6b3289edb772e78035a6b52946b
AravindhKKS <94345681+AravindhKKS@users.noreply.github.com>
2024-02-02 15:43:44 +0530

    Update cartActions.js

--- src/store/cartActions.js
+++ src/store/cartActions.js
@@ -1,39 +1,39 @@
 
-import { cartActions } from "./cartSlice"
+// import { cartActions } from "./cartSlice"
 
-export const fetchData = () => {
-    return async(dispatch) => {
+// export const fetchData = () => {
+//     return async(dispatch) => {
 
-    const fetchHandler = async () => {
-        const res = await fetch("https://e-commerce-b8a83-default-rtdb.firebaseio.com/products.json")
-        const data = await res.json()
-        return data
-    }
-try{
-    const cartData = await fetchHandler()
-dispatch(cartActions.replaceToData(cartData))
-}catch(err){
-    console.log(err)
-}
-    }
-}
+//     const fetchHandler = async () => {
+//         const res = await fetch("https://e-commerce-b8a83-default-rtdb.firebaseio.com/products.json")
+//         const data = await res.json()
+//         return data
+//     }
+// try{
+//     const cartData = await fetchHandler()
+// dispatch(cartActions.replaceToData(cartData))
+// }catch(err){
+//     console.log(err)
+// }
+//     }
+// }
 
-export  const sendCartData = (cart) => {
+// export  const sendCartData = (cart) => {
 
-    return async () => {
-       const sendRequest = async () => {
-                const res =  await fetch("https://e-commerce-b8a83-default-rtdb.firebaseio.com/products.json ",
-                                      {
-                                      method:'PUT',
-                                    body:JSON.stringify(cart)
-                                    }
-                                    )
-                                     await res.json()
-                 }
-     try{
-await sendRequest()
-     }catch(err){
-        console.log(err)
-        }
- }
-}
+//     return async () => {
+//        const sendRequest = async () => {
+//                 const res =  await fetch("https://e-commerce-b8a83-default-rtdb.firebaseio.com/products.json ",
+//                                       {
+//                                       method:'PUT',
+//                                     body:JSON.stringify(cart)
+//                                     }
+//                                     )
+//                                      await res.json()
+//                  }
+//      try{
+// await sendRequest()
+//      }catch(err){
+//         console.log(err)
+//         }
+//  }
+// }
