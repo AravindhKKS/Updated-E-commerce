540cc3311262a7d2b2442dbd9ac131425d7d5ae8
AravindhKKS <94345681+AravindhKKS@users.noreply.github.com>
2024-02-02 15:46:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import Register from "./Pages/Register/Register";
 import ProductPage from "./Pages/ProductPage/ProductPage";
 import SingleProduct from "./Pages/SingleProduct/SingleProduct";
 import Cart from "./Pages/Cart/Cart";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { fetchData, sendCartData } from "./store/cartActions";
+import { useSelector } from "react-redux";//useDispatch 
+// import { useEffect } from "react";
+// import { fetchData, sendCartData } from "./store/cartActions";
 
 
 
 function App() {
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
     const isLoggedIn = useSelector((state) => state.auth.isLogged)
-    const cart = useSelector((state) => state.cart)
-useEffect(() => {
-dispatch(fetchData())
-},[dispatch])
+    // const cart = useSelector((state) => state.cart)
+// useEffect(() => {
+// dispatch(fetchData())
+// },[dispatch])
 
-useEffect(() => {
-if(cart.changed){
-  dispatch(sendCartData(cart))
-}
-},[dispatch, cart])
+// useEffect(() => {
+// if(cart.changed){
+  // dispatch(sendCartData(cart))
+// }
+// },[dispatch, cart])
 
   return (
     <div>
